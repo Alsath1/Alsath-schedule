73697a47e58d149eb7eb18e03a72f7c2acf213b0
unknown <fiedrstar@gmail.com>
2022-12-02 20:40:18 +0300

    init

---
+++ index.js
@@ -0,0 +1,61 @@
+import schedule from "./schedule.js";
+
+window.addEventListener('DOMContentLoaded', () => {
+  //Добавляем плагин isoWeek
+  dayjs.extend(window.dayjs_plugin_isoWeek)
+
+  //Получаем элементы со страницы, текущий день недели и номер текущей недели
+  const scheduleList = document.querySelector('.schedule-list')
+  const dayOfWeekElement = document.querySelector('.day-of-week')
+  const currentDay = dayjs().isoWeekday()
+  const currentWeek = dayjs().isoWeek()
+
+  const dayOfWeek = [
+    'Понедельник',
+    'Вторник',
+    'Среда',
+    'Четверг',
+    'Пятница',
+    'Суббота',
+    'Воскресенье'
+  ]
+
+  schedule.forEach(({dayName, lectures}) => {
+    if (dayName === dayOfWeek[currentDay - 5]) {
+      //Вставляем в HTML день недели
+      dayOfWeekElement.textContent = dayName
+
+      lectures.forEach(lecture => {
+          if (currentWeek % 2 === 0 && lecture.week === 'even' || lecture.week === 'both') {
+            //Если неделя четная
+            //Создаем элемент списка li
+            const liElement = document.createElement('li')
+            liElement.innerHTML = `
+                      <div>ВРЕМЯ: ${lecture.time}</div>
+                      <div>ПАРА: ${lecture.name}</div>
+                      <div>ЛЕКТОР: ${lecture.lector}</div>
+                      <div>АУДИТОРИЯ: ${lecture.classroom}</div>
+                      <div>НЕДЕЛЯ: ${lecture.week}</div>
+                    `
+            //Добавляем li в общий список scheduleList
+            scheduleList.append(liElement)
+          } else if (currentWeek % 2 === 1 && lecture.week === 'odd' || lecture.week === 'both') {
+            //Если неделя не четная
+            //Создаем элемент списка li
+            const liElement = document.createElement('li')
+            liElement.innerHTML = `
+                      <div>ВРЕМЯ: ${lecture.time}</div>
+                      <div>ПАРА: ${lecture.name}</div>
+                      <div>ЛЕКТОР: ${lecture.lector}</div>
+                      <div>АУДИТОРИЯ: ${lecture.classroom}</div>
+                      <div>НЕДЕЛЯ: ${lecture.week}</div>
+                    `
+            //Добавляем li в общий список scheduleList
+            scheduleList.append(liElement)
+          }
+        }
+      )
+    }
+
+  })
+})
---
+++ schedule.js
@@ -0,0 +1,160 @@
+const schedule = [
+  {
+    dayName: 'Понедельник',
+    lectures: [
+      {
+        name: 'Обществознание',
+        time: '09:00-10:30',
+        lector: 'Кузнецов Н.В',
+        classroom: 'Ауд. 206',
+        week: 'odd',
+      },
+      {
+        name: 'Физ-ра',
+        time: '10:40-12:10',
+        lector: 'Аринова К.И',
+        classroom: 'Спорт Зал',
+        week: 'both',
+      },
+      {
+        name: 'Математика',
+        time: '12:30-14:00',
+        lector: 'Погонышева В.Н',
+        classroom: 'Ауд. 116',
+        week: 'both',
+      },
+      {
+        name: 'Русский язык',
+        time: '14:10-15:40',
+        lector: 'Гунченко А.В',
+        classroom: 'Ауд. 313',
+        week: 'both',
+      },
+    ]
+  },
+  {
+    dayName: 'Вторник',
+    lectures: [
+      {
+        name: 'Астрономия',
+        time: '14:10-15:40',
+        lector: 'Кокко Д.А',
+        classroom: 'Ауд. 302',
+        week: 'both',
+      },
+      {
+        name: 'Основы безопастности жизнедеятельности',
+        time: '16:00-17:30',
+        lector: 'Накущенко К.В',
+        classroom: 'Ауд. 217',
+        week: 'both',
+      },
+    ]
+  },
+  {
+    dayName: 'Среда',
+    lectures: [
+      {
+        name: 'Математика',
+        time: '09:00-10:30',
+        lector: 'Погонышева В.Н',
+        classroom: 'Ауд. 116',
+        week: 'both',
+      },
+      {
+        name: 'Физика',
+        time: '10:40-12:10',
+        lector: 'Лавринович К.В',
+        classroom: 'Ауд. 120',
+        week: 'both',
+      },
+      {
+        name: 'Иностранный язык',
+        time: '12:30-14:00',
+        lector: '',
+        classroom: '',
+        week: 'both',
+      },
+    ]
+  },
+  {
+    dayName: 'Четверг',
+    lectures: [
+      {
+        name: 'Информатика',
+        time: '09:00-10:30',
+        lector: 'Бурылов В.С',
+        classroom: 'Ауд. 218',
+        week: 'even',
+      },
+      {
+        name: 'Информатика',
+        time: '09:00-10:30',
+        lector: 'Бурылов В.С',
+        classroom: 'Ауд. 218',
+        week: 'both',
+      },
+      {
+        name: 'История',
+        time: '12:30-14:00',
+        lector: 'Панова О.В',
+        classroom: 'Ауд. 118',
+        week: 'both',
+      },
+    ]
+  },
+  {
+    dayName: 'Пятница',
+    lectures: [
+      {
+        name: 'Физ-ра',
+        time: '12:30-14:00',
+        lector: 'Аринова К.И',
+        classroom: 'Спорт Зал',
+        week: 'both',
+      },
+      {
+        name: 'Литература',
+        time: '12:30-14:00',
+        lector: 'Гунченко А.В',
+        classroom: 'Ауд. 302',
+        week: 'odd',
+      },
+      {
+        name: 'Физика',
+        time: '14:10-15:40',
+        lector: 'Лавринович К.В',
+        classroom: 'Ауд. 305',
+        week: 'even',
+      },
+    ]
+  },
+  {
+    dayName: 'Суббота',
+    lectures: [
+      {
+        name: 'Литература народов России',
+        time: '10:40-12:10',
+        lector: 'Гунченко А.В',
+        classroom: 'Ауд. 217',
+        week: 'both',
+      },
+      {
+        name: 'Литература народов России',
+        time: '12:30-14:00',
+        lector: 'Гунченко А.В',
+        classroom: 'Ауд. 217',
+        week: 'both',
+      },
+      {
+        name: 'Математика',
+        time: '14:10-15:40',
+        lector: 'Погонышева В.Н',
+        classroom: 'Ауд. 116',
+        week: 'both',
+      },
+    ]
+  },
+]
+
+export  default schedule
